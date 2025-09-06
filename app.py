7de13452cbd6e80d183d0f9f9d66eeba094ab802
Manav <vaikunth2213@gmail.com>
2025-09-06 12:03:28 +0530

    try

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def home():
 def signup():
     return render_template('signup.html')
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
